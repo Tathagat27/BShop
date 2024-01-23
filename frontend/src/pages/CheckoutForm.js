2f11825b584a05bd47c7805d0e10b7d3f67ccfab
Tathagat27 <tat27302@gmail.com>
2024-01-23 09:55:02 +0530

    deployment changes

--- frontend/src/pages/CheckoutForm.js
+++ frontend/src/pages/CheckoutForm.js
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `http://localhost:3000/order-success/${currentOrder.id}`,
+        return_url: `https://b-shop-dr04cz2ss-tathagat27.vercel.app/order-success/${currentOrder.id}`,
       },
     });
 
